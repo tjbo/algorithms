19380b3d503d7d4b3b62f00c3e83d27facd87545
tjbo <tom@prototypable.ca>
2021-08-18 13:04:22 -0500

    validAnagram

---
+++ validAnagram.js
@@ -0,0 +1,30 @@
+function validAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false
+  }
+
+  let str1FrequencyCounter = {}
+  let str2FrequencyCounter = {}
+
+  for (let key of str1) {
+    str1FrequencyCounter[key] = (str1FrequencyCounter[key] || 0) + 1
+  }
+
+  for (let key of str2) {
+    str2FrequencyCounter[key] = (str2FrequencyCounter[key] || 0) + 1
+  }
+
+  for (let key in str1FrequencyCounter) {
+    if (str1FrequencyCounter[key] !== str2FrequencyCounter[key]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+console.log(validAnagram('anagram', 'nagaram')) // true
+console.log(validAnagram('rat', 'car')) // false
+console.log(validAnagram('test', 'test5'))
+console.log(validAnagram('aaz', 'zza'))
+console.log(validAnagram('awesome', 'awesom'))
