de0c63c7b0c0dafe43c1c66e0eb7d4596f86c758
tjbo <tom@prototypable.ca>
2021-08-18 17:21:35 -0500

    count unique values

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -1,21 +1,20 @@
 // multiple pointers pattern
+// both these pointers start at start but could start anywhere depending on what you are doing
 // assume input is always sorted
 function countUniqueValues(arr) {
   let leftPointerIndex = 0
   let rightPointerIndex = 1
-  let uniqueValues = 0
 
   while (rightPointerIndex < arr.length) {
-    if (arr[leftPointerIndex] !== arr[rightPointerIndex]) {
-      uniqueValues++
-      leftPointerIndex = rightPointerIndex
-      rightPointerIndex++
+    if (arr[leftPointerIndex] === arr[rightPointerIndex]) {
+      arr.splice(leftPointerIndex, 1)
     } else {
+      leftPointerIndex = rightPointerIndex
       rightPointerIndex++
     }
   }
 
-  return uniqueValues
+  return arr.length
 }
 
 console.log(countUniqueValues([1, 1, 1, 1, 1, 1, 2])) // returns 2
