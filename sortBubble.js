82b51f3e24baf80910186986f28d7f4b764ce6ee
tjbo <tom@prototypable.ca>
2021-08-31 16:32:31 -0500

    remove unused var

--- sortBubble.js
+++ sortBubble.js
@@ -6,13 +6,10 @@ const list = [1, 2, 6, 99, 97, 76, 33, 44, 22, 11, 3, 4]
 
 function bubbleSort(arr) {
   let isDirty = {}
-  let noSwaps = true
   for (let i = arr.length; i >= 0; i--) {
-    noSwaps = true
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1)
-        noSwaps = false
       }
     }
     if (!isDirty[i]) {
