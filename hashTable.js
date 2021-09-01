3c20c8936ba977889024119a9af44ec4c13dc1ee
tjbo <tom@prototypable.ca>
2021-09-01 16:29:38 -0500

    update hash table

--- hashTable.js
+++ hashTable.js
@@ -1,11 +1,18 @@
-// hash tables are used to store key/value pairs
-// unlike arrays hash tables are fast all of the following
+// 1. hash tables are used to store key/value pairs
+// 2. unlike arrays hash tables are fast for all of the following
 // operations: finding, adding and removing
-// js has objects and maps for hash tables
-// js has no natives for a hashing function
-// objects have some restrictions but are basically hash tables
-// in this example we will use an array to build our own hash table
-// soley to understand how they work
+// 3. js has objects and maps which can be used for hash tables
+// 4. js has no natives for a hashing function
+// 5. objects have some restrictions but are basically hash tables
+// 6. in this example we will use an array to build our own hash table
+// soley to understand the fundamentals
+// 7. Big O (average)
+// insert = O(1)
+// deletion = O(1)
+// access = O(1)
+// 8. If your hash function sucks Big O will be O(N), better to just use
+// a pre-existing hashing function
+// 9. A good hash should be fast, distribute keys uniformly, and be deterministic
 
 class HashTable {
   constructor(size = 53) {
@@ -21,6 +28,18 @@ class HashTable {
       total = (total * WEIRD_PRIME + value) % this.keyMap.length
     }
     return total
+  }
+
+  keys() {
+    return this.keyMap.flatMap((arr) => {
+      return arr.flat()[0]
+    })
+  }
+
+  values() {
+    return this.keyMap.flatMap((arr) => {
+      return arr.flat()[1]
+    })
   }
 
   get(key) {
@@ -42,10 +61,11 @@ class HashTable {
 
 const table = new HashTable()
 table.set('blue', '#cc9000')
-table.set('blue', '#cc1111')
-table.set('green', '#cc1111')
+table.set('orange', '#cc1111')
+table.set('green', '#cc2222')
 table.set('red', '#cc0000')
 
-const key = table.get('red')
+const keys = table.keys()
+const values = table.values()
 
 debugger
